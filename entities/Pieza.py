70c394f9f3a039ce84f69b6179b93b19ab861063
PipeLica23 <felilicandro@gmail.com>
2025-05-26 16:14:24 -0300

    pieza y requerimiento

--- entities/Pieza.py
+++ entities/Pieza.py
@@ -1,7 +1,8 @@
 class Pieza():
-    def __init__(self,codigo_pieza,descripcion,costo,cantidad):
+    def __init__(self,codigo_pieza,descripcion,costo,cantidad,lote,reposiciones):
         self.code=codigo_pieza
         self.desc=descripcion
         self.cost=costo
         self.cantidad=cantidad
-    
+        self.lote=lote
+        self.reposicion=reposiciones
---
+++ entities/Requerimiento.py
@@ -0,0 +1,5 @@
+class Requerimiento():
+    def __init__(self,maquina,pieza,cantidad):
+        self.pieza=pieza.codigo
+        self.maquina=maquina.codigo
+        self.cantidad=cantidad
