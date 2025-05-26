d213bbba22df6877951b2dddbf2463abdb4dda82
PipeLica23 <felilicandro@gmail.com>
2025-05-26 15:48:59 -0300

    Maquina y Pieza

---
+++ entities/Maquina.py
@@ -0,0 +1,18 @@
+class Maquina():
+    def __init__(self,codigo_maquina,descripcion,requerimiento):
+        self.code=codigo_maquina
+        self.requerimiento=requerimiento
+        self.descripcion=descripcion
+
+    def disponibilidad(self,disponible):
+        if self.cantidad>=disponible:
+            return True
+        else:
+            return False
+        
+    def agregar_requerimiento(self,nuevo_req):
+        self.requerimiento.append(nuevo_req)
+    
+    def costo_produccion(self):
+        self.requerimiento
+        
---
+++ entities/Pieza.py
@@ -0,0 +1,7 @@
+class Pieza():
+    def __init__(self,codigo_pieza,descripcion,costo,cantidad):
+        self.code=codigo_pieza
+        self.desc=descripcion
+        self.cost=costo
+        self.cantidad=cantidad
+    
