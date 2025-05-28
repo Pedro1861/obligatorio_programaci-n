f91ca456210a0944a370504aef9ae0c20291f5c1
PipeLica23 <felilicandro@gmail.com>
2025-05-28 16:54:23 -0300

    Varias Cosas, hay que perfeccionar todavia

--- entities/Sistema.py
+++ entities/Sistema.py
@@ -1,84 +1,90 @@
-import random
+
 from Pieza import Pieza
 from exceptions.Errores import FueraDeRango
 from Requerimiento import Requerimiento
-
+from Maquina import Maquina
+from Cliente import Persona, Empresa
 
 ###REGISTROS###
 
-codigos_generados = set()
+codigos_generados = 0
 
-def generar_codigo(longitud=6):
-    while True:
-        codigo = ''.join(random.choices('0123456789', k=longitud))
-        if codigo not in codigos_generados:
-            codigos_generados.add(codigo)
-            return codigo
+def generar_codigo():
+    codigo= codigos_generados+1
+    codigos_generados=codigo
+    return codigo
 
 
 
-
-#PIEZA#
-lista_piezas=[]
-def registrar_pieza(lista_piezas):
-    desc_pieza=input("Brinda una descripcion de la pieza: ")
-    for i in lista_piezas:
-        if i.desc==desc_pieza:
-            nueva_desc=int(input("ERROR:La pieza ya existe.\n Si desea poner otra descripcion, ingrese 1\n Si desea abortar la operacion ingrese 2"))
-            if nueva_desc!=1 and nueva_desc!=2:
-                raise FueraDeRango
-
-            if nueva_desc==1:
-                desc_pieza=input("Ingrese una descripcion valida: ")
-                if desc_pieza==i.desc:
-                    print("ERROR: La descrpcion sigue siendo igual, operacion terminada")
-                    break
-
-            elif nueva_desc==2:
-                break
-
-    costo_pieza=int(input("Brinda costo por unidad de la pieza: "))
-    lote_pieza=int(input("Brinda el tamanio del lote de reposicion: "))
-    cantidad_pieza=int(input("Brinda un cantidad inicial de piezas: "))
-    pieza0=Pieza(generar_codigo(),desc_pieza,costo_pieza,cantidad_pieza,lote_pieza)
-    lista_piezas.append(pieza0)
+class Sistema():
     
+    lista_piezas=[]
+    lista_maquinas=[]
+    lista_pedidos=[]
+    lista_clientes=[]
 
 
-#MAQUINA#
-lista_maquinas=[]
-def registrar_maquina(lista_maquinas):
-    lista_requerimientos=[]
-    print("Comenzemos con sus requerimientos...\n")
-    print(lista_piezas)
-    code=int(input("ingrese el codigo de la pieza requerida: "))
-    nuevo_requerimiento=1
-    while nuevo_requerimiento==1:
-        nueva_pieza=0
-        for i in lista_piezas:
-            if i.codigo==code:
-                nueva_pieza=i
-        while nueva_pieza==0:
-            print("El codigo no pertenece a ninguna pieza")
-            code=int(input("ingrese el codigo de la pieza requerida: "))
+    
+    ####PIEZA####
+    
+    def registrar_pieza(self,codigo,desc_pieza,costo_pieza,cantidad_pieza,lote_pieza):
+
+        pieza0=Pieza(codigo,desc_pieza,costo_pieza,cantidad_pieza,lote_pieza)
+        self.lista_piezas.append(pieza0)
+    
+    def listar_piezas():
+        pass
+    ####PIEZAS####
+    
+    ####MAQUINAS###
+    def registrar_maquina(lista_maquinas,lista_piezas):
+        lista_requerimientos=[]
+        print("Comenzemos con sus requerimientos...\n")
+        print(lista_piezas)
+        code=int(input("ingrese el codigo de la pieza requerida: "))
+        nuevo_requerimiento=1
+        while nuevo_requerimiento==1:
+            nueva_pieza=0
             for i in lista_piezas:
                 if i.codigo==code:
                     nueva_pieza=i
-        cantidad_piezas=int(input("Ingrese la cantidad de piezas necesarias: "))      
-        nuevo_requerimiento=int(input("Desea agregar un nuevo requerimiento?\n 1.SI\n 2.NO"))
-        if nuevo_requerimiento!=1 and nuevo_requerimiento!=0:
-            raise FueraDeRango
-            
-            
+            while nueva_pieza==0:
+                print("El codigo no pertenece a ninguna pieza")
+                code=int(input("ingrese el codigo de la pieza requerida: "))
+                for i in lista_piezas:
+                    if i.codigo==code:
+                        nueva_pieza=i
+            cantidad_piezas=int(input("Ingrese la cantidad de piezas necesarias: "))      
+            nuevo_requerimiento=int(input("Desea agregar un nuevo requerimiento?\n 1.SI\n 2.NO"))
+            if nuevo_requerimiento!=1 and nuevo_requerimiento!=0:
+                raise FueraDeRango
+    def listar_maquinas():
+        pass
+    ####MAQUINAS####
+                
 
 
 
 
-#CLIENTE#
-#REPOSICION#
-###LISTADOS###
-#CLIENTES#
-#PEDIDOS#
-#MAQUINAS#
-#PIEZAS#
-#CONTABILIDAD#
+    ####CLIENTE####
+    def registrar_cliente():
+        pass
+
+    def listar_cliente():
+        pass
+    ####CLIENTE####
+
+    ####REPOSICION####
+    def registrar_reposicion():
+        pass
+    ####REPOSICION####
+    
+    
+    #PEDIDOS#
+    def registrar_pedido():
+        pass
+
+    def listar_pedido():
+        pass
+   
+    #CONTABILIDAD#
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
+from exceptions.Errores import FueraDeRango
+from entities.Sistema import Sistema
+
 print("\n1. Registrar\n2. Listar\n3. Salir del sistema")
 menu=int(input("\nElija su accion a realizar: "))
-if menu<1 or menu>3:
-    raise ValueError
 while menu!=3:
-    if menu==1:
-        print("\nRegistrar:\n 1. Pieza\n 2. Maquina\n 3. Cliente\n 4. Pedido\n 5. Reposicion\n 6. Salir")
-        registrar=int(input("\nIngrese indice de elemento a registrar: "))
-        if registrar<1 or registrar>6:
-            raise ValueError
-    elif menu==2:
-        print("\nListar: \n 1. Clientes\n 2. Pedidos\n 3. Cliente\n 4. Pedido\n 5. Reposicion\n 6. Salir")
-        listar=int(input("\nIngrese indice de elemento a listar: "))
-        if listar<1 or listar>6:
-            raise ValueError
-    print("\n1. Registrar\n2. Listar\n3. Salir del sistema")
-    menu=int(input("\nElija nuevamente su accion a realizar: "))
+    if menu<1 or menu>3:
+        raise ValueError
+    while menu!=3:
+        if menu==1:
+            print("\nRegistrar:\n 1. Pieza\n 2. Maquina\n 3. Cliente\n 4. Pedido\n 5. Reposicion\n 6. Salir")
+            registrar=int(input("\nIngrese indice de elemento a registrar: "))
+            if registrar<1 or registrar>6:
+                raise ValueError
+            if registrar==1:
+                desc_pieza=input("Brinda una descripcion de la pieza: ")
+                for i in Sistema.lista_piezas:
+                    if i.desc==desc_pieza:
+                        nueva_desc=int(input("ERROR:La pieza ya existe.\n Si desea poner otra descripcion, ingrese 1\n Si desea abortar la operacion ingrese 2"))
+                        if nueva_desc!=1 and nueva_desc!=2:
+                            raise FueraDeRango
+
+                        if nueva_desc==1:
+                            desc_pieza=input("Ingrese una descripcion valida: ")
+                            while  desc_pieza==i.desc:
+                                print("ERROR: La descripcion sigue siendo igual")
+                                desc_pieza=input("Ingrese una descripcion valida: ")
+
+                        elif nueva_desc==2:
+                            break
+                            
+
+                costo_pieza=int(input("Brinda costo por unidad de la pieza: "))
+                lote_pieza=int(input("Brinda el tamanio del lote de reposicion: "))
+                cantidad_pieza=int(input("Brinda un cantidad inicial de piezas: "))
+                Sistema.registrar_pieza()
+            elif registrar==2:
+                pass
+            elif registrar==3:
+                pass
+            elif registrar==4:
+                pass
+            elif registrar==5:
+                pass
+            elif registrar==6:
+                pass
+        elif menu==2:
+            print("\nListar: \n 1. Clientes\n 2. Pedidos\n 3. Cliente\n 4. Pedido\n 5. Reposicion\n 6. Salir")
+            listar=int(input("\nIngrese indice de elemento a listar: "))
+            if listar<1 or listar>6:
+                raise ValueError
+        print("\n1. Registrar\n2. Listar\n3. Salir del sistema")
+        menu=int(input("\nElija nuevamente su accion a realizar: "))
